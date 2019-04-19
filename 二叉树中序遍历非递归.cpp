46949254c6cdc619c5fa83bb61b42a17c7f136e3
Al Gore <hulianwang_jyh@163.com>
2019-04-19 12:07:11 +0800

    非递归前序、中序遍历二叉树的正确写法

--- 二叉树中序遍历非递归.cpp
+++ 二叉树中序遍历非递归.cpp
@@ -1,46 +1,17 @@
-/*一种更优雅和更好理解的方式*/
+﻿/*一种更优雅和更好理解的方式*/
 int InOrderTraverse(BiTree T) {
 	stack<Node*> s;
 	Node* p = T;
 
 	while(p || !s.empty()) {
-		while(p) {
+		if (p) {
 			s.push(p);
 			p = p->lchild;
-		}
-		while(!s.empty()) {
+		} else {
 			p = s.top();
 			visit(p);
 			s.pop();
 			p = p->rchild;
-		}
-	}
-}
-//上面的方法不用判断T的合法性，不用先push根节点进栈，并且不会push空指针进stack
-
-
-//二叉树的中序遍历非递归排序
-void InOrderTraverse(BiTree T) {
-	if(T == nullptr) return;
-	
-	//辅助栈
-	vector<BiTNode*> S;
-	BiTNode* p = T;
-	S.push_back(p);
-	
-	while(!S.empty()) {
-		while(p) {
-			p = p->lchild;
-			S.push_back(p);
-		}
-		S.pop_back();
-		
-		//出栈，访问结点，转到右子树；
-		if(!S.empty()) {
-			p = S.pop_back();
-			Visit(p);
-			p = p->rchild;
-			S.push_back(p);
 		}
 	}
 }
--- 二叉树先序遍历非递归.cpp
+++ 二叉树先序遍历非递归.cpp
@@ -1,24 +1,4 @@
 ﻿//二叉树的先序遍历非递归算法
-
-void PreOrder(BTree root) {
-	if (root == null) return;
-	
-	//模拟栈；
-	vector<BTNode*> s;
-	auto p = root;
-	
-	while(p || !s.empty()) {
-		if(p) {
-			visit(p);
-			s.push_back(p);
-			p = p->lchild;
-		} else {
-			p = s.pop_back();
-			p = p->rchild;
-		}
-	}	
-}
-
 //更好理解的版本
 void Preorder(BiTree root) {
 	stack<const BtNode*> s;
