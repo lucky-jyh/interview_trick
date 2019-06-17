6e472ad4385f850f98502a2907c4026cfe4c2d1d
Al Gore <hulianwang_jyh@163.com>
2019-06-17 19:10:33 +0800

    Update 二叉树先序遍历非递归.cpp

--- 二叉树先序遍历非递归.cpp
+++ 二叉树先序遍历非递归.cpp
@@ -1,16 +1,17 @@
 ﻿//二叉树的先序遍历非递归算法
-//更好理解的版本
-void Preorder(BiTree root) {
-	stack<const BtNode*> s;
-	if (root != nullptr) 
-		s.push(root);
-	
-	while (!s.empty()) {
-		const BtNode *p = s.top();
-		s.pop();
-		visit(p);
+void PreOrder(BiTree T) {
+	stack<Node*> s;
+	Node*p = T;
 
-		if (p->lchild != nullptr) s.push(p->lchild);
-		if (p->rchild != nullptr) s.push(p->rchild);
+	while(p || !s.empty()) {
+		while(p) {
+			visit(p);
+			s.push(p);
+			p = p->lchild;
+		}
+		if(!s.empty()) {
+			p = s.pop();
+			p = p->rchild;
+		}
 	}
 }
