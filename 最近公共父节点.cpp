c430cd48e21fe7d8222c7499785346e9e30128a6
Al Gore <hulianwang_jyh@163.com>
2021-05-24 01:05:08 +0800

    Update 最近公共父节点.cpp

--- 最近公共父节点.cpp
+++ 最近公共父节点.cpp
@@ -3,6 +3,7 @@ int lowestCommonAncestor(TreeNode* root, int a, int b) {
 	if (root == NULL || root->val ==a || root->val == b) {
 		return root;
 	}
+
 	TreeNode* left = lowestCommonAncestor(root->left, a, b);
 	TreeNode* right = lowestCommonAncestor(root->right, a, b);
 	if (left == NULL) {  //左子树中没有找到a,b，返回右子树的结果
@@ -11,5 +12,6 @@ int lowestCommonAncestor(TreeNode* root, int a, int b) {
 	if (right == NULL) {  //右子树中没有找到a,b，返回左子树的结果
 		return left;
 	}
+
 	return root;  //a,b分别再root左右子树的两侧
 }
